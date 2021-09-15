4ae7b3cd0e3ebb33d4c38c94f1878d0adc8d49b0
root <root@DESKTOP-5V1GL77.localdomain>
2021-09-16 01:21:02 +0530

    challenge 1

---
+++ challenge-1.js
@@ -0,0 +1,8 @@
+//Create a function `SayHello` which will accept a person's name and which will return "Hello, Name".
+
+function SayHello(name)
+{
+	return "Hello "+ name;
+}
+
+console.log(SayHello("Akshay"));
